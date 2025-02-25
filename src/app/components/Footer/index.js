3223f78101d43fc8cf7f6a476c1629396ec35a2b
SDI-sodya <saienko.dan06sdi@gmail.com>
2025-02-25 20:30:59 +0200

    Create project and first steps

---
+++ src/app/components/Footer/index.js
@@ -0,0 +1,4 @@
+// src/components/Footer.js
+export default function Footer() {
+  return <footer><p>© 2025 Все права защищены</p></footer>;
+}
---
+++ src/app/components/Header/index.js
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+  <header id="header">
+    <div className="nav-wrapper">
+      <nav className="main-nav">
+        <div className="nav-logo">
+          <Image
+          src="/images/main_logo_w.png"
+          alt="Logo"
+          // fill={true}
+          sizes="100px"
+          style={{
+              marginRight: '1em',
+              height: '100%'
+            }}
+          width={100}
+          height={100}
+          />
+          <div class="logo-text">
+            <p>Християнська церква &quot;Перемога&quot;</p>
+            <p>м. Запоріжжя</p>
+          </div>
+        </div>
+        <ul className="nav-list">
+          <li className="nav-elem">Головна</li>
+          <li className="nav-elem">Служіння</li>
+          <li className="nav-elem">Новини</li>
+          <li className="nav-elem">Галерея</li>
+          <li className="nav-elem">Контакти</li>
+          <li className="nav-elem">Про нас</li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  );
+}
