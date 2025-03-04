c0aec8e212693d0a7b34e95605b189194f5c1e93
SDI-sodya <saienko.dan06sdi@gmail.com>
2025-03-04 14:15:17 +0200

    Some changes

--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -1,11 +1,12 @@
+import styles from './Header.module.css';
 import Image from "next/image";
 
 export default function Header() {
   return (
-  <header id="header">
-    <div className="nav-wrapper">
-      <nav className="main-nav">
-        <div className="nav-logo">
+  <header id={`${styles.header}`}>
+    <div className={`${styles.navWrapper}`}>
+      <nav className={`${styles.mainNav}`}>
+        <div className={`${styles.navLogo}`}>
           <Image
           src="/images/main_logo_w.png"
           alt="Logo"
@@ -18,18 +19,18 @@ export default function Header() {
           width={100}
           height={100}
           />
-          <div className="logo-text">
+          <div className={`${styles.logoText}`}>
             <p>Християнська церква &quot;Перемога&quot;</p>
             <p>м. Запоріжжя</p>
           </div>
         </div>
-        <ul className="nav-list">
-          <li className="nav-elem">Головна</li>
-          <li className="nav-elem">Служіння</li>
-          <li className="nav-elem">Новини</li>
-          <li className="nav-elem">Галерея</li>
-          <li className="nav-elem">Контакти</li>
-          <li className="nav-elem">Про нас</li>
+        <ul className={`${styles.navList}`}>
+          <li className={`${styles.navElem}`}>Головна</li>
+          <li className={`${styles.navElem}`}>Служіння</li>
+          {/* <li className={`${styles.navElem}`}>Новини</li> */}
+          <li className={`${styles.navElem}`}>Галерея</li>
+          <li className={`${styles.navElem}`}>Контакти</li>
+          <li className={`${styles.navElem}`}>Про нас</li>
         </ul>
       </nav>
     </div>
