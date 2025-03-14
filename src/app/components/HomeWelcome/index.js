c5c4fb75d1e878fcd4da0fb6caa8b77bfd38c71e
SDI-sodya <saienko.dan06sdi@gmail.com>
2025-03-15 00:27:18 +0200

    Create new component

---
+++ src/app/components/HomeWelcome/index.js
@@ -0,0 +1,10 @@
+import styles from './HomeWelcome.module.css'
+
+export default function HomeWelcome() {
+  return(
+    <div className='wrapper'>
+      <h2 className='title'></h2>
+      <p className='text'></p>
+    </div>
+  )
+}
