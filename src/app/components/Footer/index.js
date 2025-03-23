db9236911e19801ff7d3fca50cf6b7a861e40f5a
SDI-sodya <saienko.dan06sdi@gmail.com>
2025-03-24 00:39:04 +0200

    Change global styles, add links in footer and header

--- src/app/components/Footer/index.js
+++ src/app/components/Footer/index.js
@@ -1,4 +1,43 @@
-// src/components/Footer.js
+import Link from 'next/link';
+import styles from './Footer.module.css';
+import Image from 'next/image';
+
 export default function Footer() {
-  return <footer><p>© 2025 Все права защищены</p></footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className='wrapper'>
+        <div className={styles.navWrapper}>
+          <div className={`${styles.logo}`}>
+            <Image
+            src="/images/main_logo_w.png"
+            alt="Logo"
+            // fill={true}
+            sizes="100px"
+            style={{
+              marginRight: '1em',
+              height: '100%'
+            }}
+            width={100}
+            height={100}
+            />
+            <div className={`${styles.logoText}`}>
+              <p>Християнська церква &quot;Перемога&quot;</p>
+              <p>м. Запоріжжя</p>
+            </div>
+          </div>
+          <div className={styles.navListWrapper}>
+            <ul className={`${styles.navList}`}>
+              <li className={`${styles.navElem}`}><Link className={styles.link} href='/'>Головна</Link></li>
+              <li className={`${styles.navElem}`}><Link className={styles.link} href='/'>Служіння</Link></li>
+              {/* <li className={`${styles.navElem}`}>Новини</li> */}
+              <li className={`${styles.navElem}`}><Link className={styles.link} href='/'>Галерея</Link></li>
+              <li className={`${styles.navElem}`}><Link className={styles.link} href='/'>Контакти</Link></li>
+              <li className={`${styles.navElem}`}><Link className={styles.link} href='/'>Про нас</Link></li>
+            </ul>
+          </div>
+        </div>
+        <p className={styles.copy}>© 2025 Все права защищены</p>
+      </div>
+    </footer>
+  );
 }
--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './Header.module.css';
 import Image from "next/image";
 
@@ -6,31 +7,33 @@ export default function Header() {
   <header id={`${styles.header}`}>
     <div className='wrapper'>
       <nav className={`${styles.mainNav}`}>
-        <div className={`${styles.navLogo}`}>
-          <Image
-          src="/images/main_logo_w.png"
-          alt="Logo"
-          // fill={true}
-          sizes="100px"
-          style={{
-              marginRight: '1em',
-              height: '100%'
-            }}
-          width={100}
-          height={100}
-          />
-          <div className={`${styles.logoText}`}>
-            <p>Християнська церква &quot;Перемога&quot;</p>
-            <p>м. Запоріжжя</p>
+        <Link className={styles.link} href='/'>
+          <div className={`${styles.navLogo}`}>
+            <Image
+            src="/images/main_logo_w.png"
+            alt="Logo"
+            // fill={true}
+            sizes="100px"
+            style={{
+                marginRight: '1em',
+                height: '100%'
+              }}
+            width={100}
+            height={100}
+            />
+            <div className={`${styles.logoText}`}>
+              <p>Християнська церква &quot;Перемога&quot;</p>
+              <p>м. Запоріжжя</p>
+            </div>
           </div>
-        </div>
+        </Link>
         <ul className={`${styles.navList}`}>
-          <li className={`${styles.navElem}`}>Головна</li>
-          <li className={`${styles.navElem}`}>Служіння</li>
+          <li className={`${styles.navElem}`}><Link className={styles.link} href='/'>Головна</Link></li>
+          <li className={`${styles.navElem}`}><Link className={styles.link} href='/'>Служіння</Link></li>
           {/* <li className={`${styles.navElem}`}>Новини</li> */}
-          <li className={`${styles.navElem}`}>Галерея</li>
-          <li className={`${styles.navElem}`}>Контакти</li>
-          <li className={`${styles.navElem}`}>Про нас</li>
+          <li className={`${styles.navElem}`}><Link className={styles.link} href='/'>Галерея</Link></li>
+          <li className={`${styles.navElem}`}><Link className={styles.link} href='/'>Контакти</Link></li>
+          <li className={`${styles.navElem}`}><Link className={styles.link} href='/'>Про нас</Link></li>
         </ul>
       </nav>
     </div>
